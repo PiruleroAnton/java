34b20e0c8e792d71815c1a09d52dac1f8320d277
PiruleroAnton <piruleroanton2017@gmail.com>
2022-02-14 10:36:52 +0100

    matriz inversa añadida

---
+++ matrices/src/matrices/matriz_inversa.java
@@ -0,0 +1,18 @@
+package matrices;
+
+public class matriz_inversa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejemplo de matriz en java:");
+		System.out.println(" ");
+		
+		int[] matriz = {1,2,3,4,5};
+		
+		for (int f=4;f>=0;f--) { // la matriz empieza en 0
+			
+			System.out.println("Valor matriz indice: "+f+" valor: "+matriz[f]);
+		}
+	}
+
+}
