33aaff202c53ad76f17a62628a5c5ea0ab723ed8
PiruleroAnton <piruleroanton2017@gmail.com>
2022-02-14 10:22:02 +0100

    update con matriz real

--- matrices/src/matrices/ejemplo1.java
+++ matrices/src/matrices/ejemplo1.java
@@ -5,7 +5,17 @@ public class ejemplo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		System.out.println("funciona");
+		System.out.println("Ejemplo de matriz en java:");
+		System.out.println(" ");
+		
+		int[] matriz = {1,2,3,4,5};
+		
+		for (int f=0;f<=4;f++) { // la matriz empieza en 0
+			
+			System.out.println("Valor matriz indice: "+f+" valor: "+matriz[f]);
+		}
+		
+		
 
 	}
 
